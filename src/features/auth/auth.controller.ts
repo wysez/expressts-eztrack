d61c9f0c74f4812aa1f3a1136b24bc6409a25c84
fltzr <josh.salinas.j@outlook.com>
2024-01-11 01:21:59 -0500

    Add auth controller and routes

---
+++ src/features/auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const pageload = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {};
+export const login = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {};
+export const logout = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {};
+export const userinfo = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {};
+export const refresh = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {};
--- src/features/auth/auth.routes.ts
+++ src/features/auth/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import { login, logout, pageload, refresh, userinfo } from './auth.controller';
 
 export const authRoutes = Router();
 
 authRoutes
-  .post('/pageload')
-  .post('/login')
-  .post('/logout')
-  .post('/userinfo')
-  .post('/refresh');
+  .post('/pageload', pageload)
+  .post('/login', login)
+  .post('/logout', logout)
+  .post('/userinfo', userinfo)
+  .post('/refresh', refresh);
