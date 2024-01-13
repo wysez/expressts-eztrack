db7a5001125d8b184e2cbf47a3edd74413dc0099
fltzr <josh.salinas.j@outlook.com>
2024-01-13 15:46:24 -0500

    Refactor OIDC client configuration

--- src/core/utils/oidc-client.ts
+++ src/core/utils/oidc-client.ts
@@ -1,21 +1,15 @@
 import { Client, Issuer } from 'openid-client';
-import {
-  OIDC_CLIENT_ID,
-  OIDC_CLIENT_SECRET,
-  OIDC_ISSUER,
-  OIDC_REDIRECT_URI,
-  OIDC_RESPONSE_TYPE,
-} from '../config';
+import { env } from '@config';
 
 let oidc: Client | null = null;
 
 export const initializeOpenIDConnectClient = async () => {
-  const issuer = await Issuer.discover(OIDC_ISSUER);
+  const issuer = await Issuer.discover(env.OIDC_ISSUER);
   oidc = new issuer.Client({
-    client_id: OIDC_CLIENT_ID,
-    client_secret: OIDC_CLIENT_SECRET,
-    redirect_uris: [OIDC_REDIRECT_URI],
-    response_types: [OIDC_RESPONSE_TYPE],
+    client_id: env.OIDC_CLIENT_ID,
+    client_secret: env.OIDC_CLIENT_SECRET,
+    redirect_uris: [env.OIDC_REDIRECT_URI],
+    response_types: [env.OIDC_RESPONSE_TYPE],
     token_endpoint_auth_method: 'client_secret_basic',
   });
 };
