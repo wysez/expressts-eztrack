3552cf74ba8dbbe96966af57a16b6ce0337ab16f
fltzr <josh.salinas.j@outlook.com>
2024-01-15 00:10:30 -0500

    Refactor User table schema in user.ts

--- src/core/database/schema/user.ts
+++ src/core/database/schema/user.ts
@@ -1,5 +1,6 @@
 import {
   date,
+  index,
   pgEnum,
   pgTable,
   text,
@@ -16,19 +17,29 @@ export const genderEnum = pgEnum('gender', [
 ]);
 
 // Define User table
-export const users = pgTable('users', {
-  id: uuid('id').primaryKey().notNull().defaultRandom(),
-  username: text('username').unique().notNull(),
-  email: text('email').unique().notNull(),
-  password: text('password').notNull(),
-  firstName: text('first_name').notNull(),
-  lastName: text('last_name').notNull(),
-  birthDate: date('birth_date', { mode: 'date' }).notNull(),
-  gender: genderEnum('gender').default('prefer_not_to_say'),
-  zipCode: text('zip_code').notNull(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at').notNull().defaultNow(),
-});
+export const users = pgTable(
+  'users',
+  {
+    id: uuid('id').primaryKey().notNull().defaultRandom(),
+    username: text('username').unique().notNull(),
+    email: text('email').unique().notNull(),
+    password: text('password').notNull(),
+    firstName: text('first_name').notNull(),
+    lastName: text('last_name').notNull(),
+    birthDate: date('birth_date', { mode: 'string' }).notNull(),
+    gender: genderEnum('gender').default('prefer_not_to_say'),
+    zipCode: text('zip_code').notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true })
+      .notNull()
+      .defaultNow(),
+    updatedAt: timestamp('updated_at', { withTimezone: true })
+      .notNull()
+      .defaultNow(),
+  },
+  table => ({
+    username_idx: index('username_idx').on(table.username),
+  }),
+);
 
 // Infer types from schema
 export const User = users.$inferSelect;
